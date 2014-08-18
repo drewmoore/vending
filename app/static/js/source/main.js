b53a3579511131ce0f3bb0f0be5fdc178dc1152c
Drew Moore <teloaster@yahoo.com>
2014-08-18 02:24:04 -0500

    Currency can be added to purchase queue. Value display in Coin display. Coin display formated for numbers and strings.

--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -2,6 +2,9 @@
 
   'use strict';
 
+  var CurrencyValue = {};
+  var PurchaseQueue = {};
+
   $(document).ready(initialize);
 
   function initialize(){
@@ -11,7 +14,8 @@
   }
 
   function loadWallet(){
-    // Starts off every denomination in wallet with a count of 100, or whatever we want to specify.
+    // Starts off every denomination in wallet with a specified count. Define global currency denomination values.
+    // Load Purchase Queue with initial values.
     var bankForEach = 30;
     var denominationDivs = $('.currency');
 
@@ -19,7 +23,10 @@
       var type = $(div).attr('data-type');
       var $countDisplay = $('.currency-in-wallet[data-type="' + type + '"]');
       $countDisplay.text(bankForEach.toString());
+      CurrencyValue[type] = $(div).attr('data-value') - 0;
+      PurchaseQueue[type] = 0;
     });
+    PurchaseQueue.value = 0;
   }
 
   function defineEventHandlers(){
@@ -29,29 +36,17 @@
   function currencyClick(){
     var self = this;
     var type = $(self).attr('data-type');
-    var value = $(self).attr('data-value');
     var $element = $('.currency-in-wallet[data-type="' + type + '"]');
     var domCount = parseInt($element.text());
 
-    // Determine if there is sufficient space in machine to add a given coin or bill
+    // Determine if there is sufficient space in machine to add a given coin or bill and that the user has sufficient money in wallet.
+    // Adjust coin count display.
     if(bankHasOverhead(type) && domCount > 0){
-      // Decrement wallet by denomination. Adjust overhead by denomination. Add to purchase queue by denomination.
-      var $dataElement;
-      if(isPaper(type)){
-        $dataElement = $('.slots-left[data-type="paperBill"]');
-      } else {
-        $dataElement = $('.slots-left[data-type="' + type + '"]');
-      }
-      var overhead = parseInt($dataElement.attr('data-overhead'));
-      domCount -= 1;
-      overhead -= 1;
-      $element.text(domCount.toString());
-      $dataElement.attr('data-overhead', overhead.toString());
-
-      console.log('bank has overhead and domCount > 0', '\n');
+      decrementWallet(type, $element, domCount);
+      incrementPurchaseQueue(type);
+      adjustCoinDisplay(PurchaseQueue.value);
     } else if(!bankHasOverhead(type)){
-
-      console.log('bank is full, damnit', '\n');
+      adjustCoinDisplay(type + ' slot full');
     }
 
     // Determines if there is sufficient space for a given denomination, different rules apply to paper bills
@@ -70,7 +65,28 @@
       console.log('bankHasOverhead called: ', type, $dataElement, overhead, '\n');
     }
 
-    console.log('currencyClick called: ', self, type, value, '\n');
+    function decrementWallet(type, $element, domCount){
+      // Decrement wallet by denomination. Adjust overhead by denomination. Add to purchase queue by denomination.
+      var $dataElement;
+      if(isPaper(type)){
+        $dataElement = $('.slots-left[data-type="paperBill"]');
+      } else {
+        $dataElement = $('.slots-left[data-type="' + type + '"]');
+      }
+      var overhead = parseInt($dataElement.attr('data-overhead'));
+      domCount -= 1;
+      overhead -= 1;
+      $element.text(domCount.toString());
+      $dataElement.attr('data-overhead', overhead.toString());
+    }
+
+    function incrementPurchaseQueue(type) {
+      PurchaseQueue[type] ++;
+      PurchaseQueue.value += (CurrencyValue[type] - 0);
+    }
+
+
+    console.log('currencyClick called: ', self, type, CurrencyValue, PurchaseQueue, '\n');
   }
 
   function isPaper(type){
@@ -82,6 +98,17 @@
     }
 
     console.log('isPaper called: ', type, $dataElement, '\n');
+  }
+
+  function adjustCoinDisplay(input){
+    var $coinDisplay = $($('#coin-display > h1')[0]);
+    var newText = '';
+    if((typeof input) !== 'string'){
+      newText = input.toFixed(2);
+    } else {
+      newText = input.toUpperCase();
+    }
+    $coinDisplay.text(newText);
   }
 
 
